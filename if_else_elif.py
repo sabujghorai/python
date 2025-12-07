9660d010cd7fe5096f656901b1123e206aba1345
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-07 12:09:44 +0530

    Add traffic light signal logic
    Implement traffic light signals using if, elif, and else statements.

---
+++ if_else_elif.py
@@ -0,0 +1,11 @@
+#traffic light signals using if,elif,else
+light = input("Enter the colour of the light in small letter :")
+if(light == "red"):
+    print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+    print("look") # the 4 spaces are called indentation
+else:
+    print("please enter a valid colour..")
+  
