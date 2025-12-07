069653d323a923c693608181fc108e262ecb3e74
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-07 12:08:39 +0530

    Create str_func.py

---
+++ str_func.py
@@ -0,0 +1,21 @@
+# endswith function...
+str  = "hey hello how are you"
+print(str.endswith("u"))
+
+# capitalized function...
+a = "good night users"
+print(a.capitalize()) #---> capitalized the 1st char
+
+# replace function...
+b = "i am writing this program at the night time at 2 AM"
+print(b.replace("a","o"))
+print(b.replace("writing","reading"))
+
+# find function...
+c = "hello linked in users"
+print(c.find("k"))
+
+# count function...
+d = "you are good and you are cute"
+print(d.count("you"))
+print(d.count("a")) # counts how many times your word or letter has repeated
