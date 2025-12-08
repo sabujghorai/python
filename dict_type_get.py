9b4550fb0c3fdd9727ae13b4574eab81d5333961
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-08 16:55:36 +0530

    Create dict_type_get.py

---
+++ dict_type_get.py
@@ -0,0 +1,10 @@
+student = {
+  "name" : "sabuj ghorai",
+  "subject" : {
+    "phy" : 87,
+    "chem" : 89,
+    "math" : 87
+  }
+}
+print(student["name"])
+print(student.get("name")) # both will print 'sabuj ghorai' stored in name 
