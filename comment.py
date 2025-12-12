c57a9990c49be76d6b9afa4f496b21feaf9c0869
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-12 17:15:37 +0530

    Update comment.py

--- comment.py
+++ comment.py
@@ -1,2 +1,4 @@
 print("hello world")
 # hey hello how are you users i am writing this line for fun this line does not have any meaning 
+print("hello world")
+''' hey user i am using the triple cotes for multi line comment'''
