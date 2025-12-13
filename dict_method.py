031c9d5de633971192e41d2fbf3c1b7ecef0e2af
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-14 00:41:22 +0530

    Create dict_method.py

---
+++ dict_method.py
@@ -0,0 +1,9 @@
+student = {
+  "name" : "Sabuj Ghorai",
+  "subject" : {  # nested dictionary
+    "phy" : 95,
+    "chem" : 91,
+    "maths" : 93
+  }
+}
+print(student.keys()) #prints the key keys ('name') and ('subject')...
