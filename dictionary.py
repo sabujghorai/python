0304d8c36e8ab130b6caff69c3872ffc6132316a
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-13 22:49:42 +0530

    Fix duplicate key in dictionary and update comments
    Corrected duplicate key 'marks' and fixed comments.

--- dictionary.py
+++ dictionary.py
@@ -10,6 +10,8 @@ student = {
   "learning" : "python",
   "subjects" : ["chemistry","English","C programming","hermony","Design THinking"], # we can store a list also
   "marks" : (95,91,97,85,87), # we can store tuple also
+  "is_adult" : True, # we cal store a boolean value
+  "marks" : (987,73,923) # we cannot assign same key value if we use then it will not run..
   
 }
 print(dict["name"])
