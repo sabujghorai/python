763701d56d94fc34c808f4eb233b5b037e1727d6
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-13 22:47:31 +0530

    Create dictionary1.py

---
+++ dictionary1.py
@@ -0,0 +1,12 @@
+dict = {
+  "name" : "sanbuj ghorai ",
+  "age" : 21,
+  "learning" : "python",
+  "subjects" : ["chemistry","English","C programming","hermony","Design THinking"], # we can store a list also
+  "marks" : (95,91,97,85,87), # we can store tuple also
+  
+}
+print(dict["name"])
+print(dict["age"])
+print(dict["learning"])
+print()
