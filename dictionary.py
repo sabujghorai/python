e930eede83320f5d2ffbefef08ca2c06975fd21d
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-13 22:48:14 +0530

    Add student dictionary and print details
    Added a new dictionary with student details and printed its values.

--- dictionary.py
+++ dictionary.py
@@ -3,7 +3,19 @@ student = {
   "subject" : {
     "phy" : 87,
     "chem" : 89,
-    "math" : 87
+    "math" : 87,
+    dict = {
+  "name" : "sanbuj ghorai ",
+  "age" : 21,
+  "learning" : "python",
+  "subjects" : ["chemistry","English","C programming","hermony","Design THinking"], # we can store a list also
+  "marks" : (95,91,97,85,87), # we can store tuple also
+  
+}
+print(dict["name"])
+print(dict["age"])
+print(dict["learning"])
+print()
   }
 }
 print(len(student))
