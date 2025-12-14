ed6a92b2113c80f4fbbde3e9fe267aa902d19346
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-14 17:59:29 +0530

    Update dict_type.py

--- dict_type.py
+++ dict_type.py
@@ -7,3 +7,14 @@ student = {
   }
 }
 print(student.values())
+print(len(student)) # means total number of key value pairs...
+
+# we can use typecasting inside the dictionary also 
+print(list(student.keys()))
+
+print(student.items()) # returns all (key , values ) pairs as tuple.. in parenthesis
+
+print(student.get("subject")) # returns the key according to the value...
+
+student.update({"eng" : 87}) # use to update inside the dict.. and the new dict should be in curly brckets
+print(student )
