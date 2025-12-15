859e1699eeb0bed0abbee5bf6348685014aeb078
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-15 11:19:18 +0530

    Create set_method.py

---
+++ set_method.py
@@ -0,0 +1,16 @@
+roll = {"hello","world","sabuj"}
+roll.add(1) # we can add one element by one parentheses
+roll.add((2,3)) # we can add multiple elements by double parentheses
+print(roll)
+
+# remove method
+roll.remove("hello") # removes the element 34
+print(roll)
+# pop method
+
+print(roll.pop())
+print(roll.pop())
+
+# clear method
+roll.clear()
+print(roll)
