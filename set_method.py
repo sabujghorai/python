66cdd14d2c0715c2d0bc55bf332394ed8dd21f99
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-15 12:48:03 +0530

    Update set_method.py

--- set_method.py
+++ set_method.py
@@ -14,3 +14,9 @@ print(roll.pop())
 # clear method
 roll.clear()
 print(roll)
+
+set1 = {2,3,4,5}
+set2 = {4,5,6,7}
+print(set1.union(set2)) # combine both set valurs and return new (prints 2,3,4,5,6,7)
+print(set1.intersection(set2)) # combine common values and return new (prints 4,5)
+# set1.union(set2) = set2.union(set1)
