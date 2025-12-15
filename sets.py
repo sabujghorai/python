3c8856a5c0392628789f53fa484705da82641787
sabujghorai <ghoraisabuj4@gmail.com>
2025-12-15 10:42:02 +0530

    Update sets.py

--- sets.py
+++ sets.py
@@ -2,3 +2,6 @@ roll_number = {281,291,286,294}
 print(roll_number)
 print(type(roll_number)) 
 print(len(roll_number)) # prints the length of the sets
+
+# we can make a empty set also 
+roll = set() # syntax of empty set
