c6d0617f59471bbdd0ab4b0f4efa6215bcf41b72
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-16 19:35:20 +0530

    Update loops_prob.py

--- loops_prob.py
+++ loops_prob.py
@@ -5,3 +5,9 @@ i = 1
 while i<=10:
   print(i*a)
   i += 1
+# this is the code for squre of numbers from one to n which is given by the user
+n = int(input("Enter a number :"))
+i = 1
+while i<=n:
+  print(i*i)
+  i += 1
