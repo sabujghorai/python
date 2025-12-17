4c1d57f7a1fc1c7842836cff37d212cb0ed2fe00
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-17 21:38:52 +0530

    Create break_continue.py

---
+++ break_continue.py
@@ -0,0 +1,11 @@
+ # BREAK STATEMENT
+
+i = 1
+while i<=5:
+  print(i)
+  if i == 4:
+     break
+  i += 1
+
+  print("loop terminate") # this print is inside the loop because tab is given at the first
+print("loop turminate") # and this print is outside the loop
