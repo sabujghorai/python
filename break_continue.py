41f2ecd312d038e10def8f7a6b7423af09e9d6b4
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-17 21:47:29 +0530

    Update break_continue.py

--- break_continue.py
+++ break_continue.py
@@ -9,3 +9,12 @@ while i<=5:
 
   print("loop terminate") # this print is inside the loop because tab is given at the first
 print("loop turminate") # and this print is outside the loop
+
+# CONTINUE STATEMENT
+i = 1
+while i <= 15:
+   if i == 3: # 3 will not print 
+      i += 1
+      continue
+   print(i)
+   i+=1
