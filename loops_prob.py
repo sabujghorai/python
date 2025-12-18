cba2308cd612c1a7646cef6678a6234718ade825
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-18 23:50:08 +0530

    Update loops_prob.py

--- loops_prob.py
+++ loops_prob.py
@@ -61,3 +61,11 @@ sum = 0
 for i in range(n+1):
   sum = sum+i
 print("the sum is:",sum)
+
+# write a program to find the factorial of first n numbers . using for loop
+n = int(input("Enter a number:"))
+fact = 1
+i = 1
+for i in range(1,n+1):
+  fact = fact*i
+print("factorial is :",fact)
