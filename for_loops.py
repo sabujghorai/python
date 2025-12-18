3c62b36ba510f6909952e3870f9a4e42486af4b8
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-18 23:22:19 +0530

    Update for_loops.py

--- for_loops.py
+++ for_loops.py
@@ -11,3 +11,13 @@ else:
    num = ( 1,4,63,35)
    for i in num:
       print(i)
+# we can use for loop in string as well
+
+name = "hello users how are you ?"
+for i in name :
+  if i == 'w':
+    print("i found")
+    break
+  print(i)
+else:
+  print("😊")
