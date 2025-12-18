cf48bde24c7b7892542f199a5d675928f21c2c67
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-18 23:25:33 +0530

    Update for_loops.py

--- for_loops.py
+++ for_loops.py
@@ -1,4 +1,4 @@
-  FOR LOOPS
+  # FOR LOOPS
 # for loop in list
 
 list = ["hello","world","sabuj","ghorai"]
@@ -21,3 +21,23 @@ for i in name :
   print(i)
 else:
   print("😊")
+
+for i in range(5): # range(stop) prints 0 to (n-1)=4
+  print(i)
+print("range 5 ends here")
+  
+for j in range(1,5): # range(start,stop)
+    print(j)
+print("range(1,5) ends here")
+
+for k in range(1,6,2): # range(start? , stop, step)
+  print(k)
+print("range(1,6,2) ends here")
+
+# PASS STATEMENT
+# pass is a null statement that does nothing . It is used as a placeholder for future code
+
+for i in range(5):
+  # we want to empty
+  pass
+print("pass statement does nothing")
