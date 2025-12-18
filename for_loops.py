c2c08f5d016968272ba9433711805c589fd3cec3
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-18 23:21:51 +0530

    Create for_loops.py

---
+++ for_loops.py
@@ -0,0 +1,13 @@
+  FOR LOOPS
+# for loop in list
+
+list = ["hello","world","sabuj","ghorai"]
+for i in list:
+   print(i)
+else:
+   print("END")
+
+   # we can use for loop in string also
+   num = ( 1,4,63,35)
+   for i in num:
+      print(i)
