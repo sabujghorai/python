1f00758de1aa5e19ede1a634bf27bd3e2020ab90
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-18 23:24:15 +0530

    Update loops_prob.py

--- loops_prob.py
+++ loops_prob.py
@@ -11,3 +11,22 @@ i = 1
 while i<=n:
   print(i*i)
   i += 1
+# this is the multiplication table using while loop
+a = int(input("Enter a number :")) 
+
+i = 1
+while i<=10:
+  print(i*a)
+  i += 1
+
+# print the elements of the following list using a for loop
+[ 1,4,9,16,25,36,49,64,81,100]
+
+list = [1,4,9,16,25,36,49,64,81,100]
+for i in list:
+  if i == 16:
+    print("stop here")
+    break
+  print(i)
+else:
+  print("happy journy")
