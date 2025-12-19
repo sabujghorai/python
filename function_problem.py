dc838e64bb7dac469f08988e469f77a9b727e73f
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-19 21:52:02 +0530

    Create function_problem.py

---
+++ function_problem.py
@@ -0,0 +1,11 @@
+# average of 3 numbers
+def average(a,b,c):
+  avg = (a+b+c)/3
+  print(avg)
+  return sum
+
+a = int(input("Enter 1st number :"))
+b = int(input("Enter 2nd number :"))
+c = int(input("Enter 3rd number :"))
+average(a,b,c)
+
