5b5d135129f47f8d19fbda0d5728da63a9255ff0
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-22 00:19:17 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -8,3 +8,15 @@ def show(n):
   show(n-1) # function call
 
 show(b)
+
+# print the factorial of a number using recursion taking from user input
+# print the factorial of a number taking user input
+
+b = int(input("Enter a number :"))
+
+def fact(a):
+  if(a == 0):
+    return 1
+  return a*fact(a-1)
+
+print("factorial of your given number is :",fact(b))
