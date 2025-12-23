8bec1fda8c03fd2abeca4723e3340b004f928858
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-23 20:45:33 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -30,3 +30,18 @@ def sum(a):
   return a+sum(a-1)
 
 print("sum of your all natural number is :",sum(b))
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+n = int(input("Enter the number of terms: "))
+
+print("Fibonacci series:")
+for i in range(n):
+    print(fibonacci(i), end=" ")
+
