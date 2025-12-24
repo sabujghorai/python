eac00c15e29f6968b01e232cae7cc7be24541dbe
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-24 19:28:22 +0530

    Create file.py

---
+++ file.py
@@ -0,0 +1,5 @@
+# I have created a file name "demo.txt" now to read the file we have to write some syntax in python
+# syntax
+f.open("demo.txt","r") # in first quotion mark we have to enter the file name and in the second we have to enter the mode
+data = f.read() # reading the file
+print(data) # printing the file
