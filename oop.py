cc87fef143adae919423ef0c89a4c1121a14a7a7
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-27 17:33:34 +0530

    Create oop.py

---
+++ oop.py
@@ -0,0 +1,9 @@
+class Car: # first letter must be capital letter 'S'tudnet
+    car_name = "Mercedese Benz"
+    model_no = 23
+    colour = "Blue"
+
+s1 = Car() # creating aa object
+print(s1.car_name)
+print(s1.model_no)
+print(s1.colour)
