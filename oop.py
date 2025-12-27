b7cd86ce78379d95bda44cc2b70cbec0860a5f52
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-27 18:38:15 +0530

    Update oop.py
    using the __inti__ function we are printing the beand and the model number

--- oop.py
+++ oop.py
@@ -7,3 +7,15 @@ s1 = Car() # creating aa object
 print(s1.car_name)
 print(s1.model_no)
 print(s1.colour)
+
+
+# __init__ function --> also call constructor
+class Car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+
+
+my_car = Car("mercedese","53")
+print(my_car.brand)
+print(my_car.model)
