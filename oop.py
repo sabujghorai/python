73c0db3494a7e97d7545d94cac475728c7d64a09
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-28 17:28:29 +0530

    Update oop.py

--- oop.py
+++ oop.py
@@ -149,3 +149,25 @@ class User:
 
 u = User("abc123")
 u.show_password()
+
+# Polymorphism (Same Method Name)
+
+class Shape:
+    def draw(self):
+        print("Drawing a shape")
+
+
+class Circle(Shape):
+    def draw(self):
+        print("Drawing a circle")
+
+
+class Square(Shape):
+    def draw(self):
+        print("Drawing a square")
+
+
+shapes = [Circle(), Square()]
+
+for s in shapes:
+    s.draw()
