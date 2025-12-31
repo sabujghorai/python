f4617a0562287e854bb129c7663abbe942e3e2ac
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-31 17:01:15 +0530

    Update oop2.py
    private(like)

--- oop2.py
+++ oop2.py
@@ -7,3 +7,13 @@ s1 = Student("sabuj")
 print(s1.name) # first it will print the name sabuj
 del s1.name # output will be an error because name attribute is already deleted
 print(s1.name)# error
+
+# Private(like) attribute & method
+class Account:
+    def __init__(self,acc_num,acc_pass):
+        self.acc_num = acc_num # public attribute
+        self.__acc_pass = acc_pass # private attribute 
+
+a = Account("123456","pass@1234")
+print(a.acc_num)
+print(a.__acc_pass)# print error 'Account' object has no attribute 'acc_pass'
