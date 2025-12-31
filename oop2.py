a13e590a291473a91daaaf7b537575dfe264c77e
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-31 17:33:47 +0530

    Update oop2.py
    if we are private an attribute by using double underscore
    then it will access through the internal function of the class 
    we cannot access the the private attribute outside the class

--- oop2.py
+++ oop2.py
@@ -21,12 +21,16 @@ a = Account("123456","pass@1234")
 print(a.acc_num)
 print(a.reset_pass()) # now it will orint the password
 print(a.__acc_pass)# print error 'Account' object has no attribute 'acc_pass'
-
 class Person:
-    __name = "hello" # creating a private class 
+    __name = "hello" # private class attribute
 
-    def __hello(): # creating a private object
-        print("hello world")
+    def __hello(self): # creating a private object
+        print("hello users")
+
+    def welcome(self):
+        self.__hello()
 
 p1 = Person()
-print(p1.__name)# it will also show an erroor because the __name ia a private attribute
+print(p1.welcome())
+# print(p1.__name)# it will also show an erroor because the __name ia a private attribute
+
