ec339b9749e92913912ed5765e46a3d6f3db84b3
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-31 16:45:32 +0530

    Update oop2.py
    print--> delete-->print
    first it will print the name 
    then it will delete the name
    now if we are try to print the name it will show an error because the name attribute does't exist

--- oop2.py
+++ oop2.py
@@ -4,5 +4,6 @@ class Student:
         self.name = name
 
 s1 = Student("sabuj")
+print(s1.name) # first it will print the name sabuj
 del s1.name # output will be an error because name attribute is already deleted
-print(s1.name)
+print(s1.name)# error
