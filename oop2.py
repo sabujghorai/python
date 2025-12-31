1d30871f42188f5e128ef8a32256d92dd8488482
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2025-12-31 17:23:12 +0530

    Update oop2.py

--- oop2.py
+++ oop2.py
@@ -21,3 +21,12 @@ a = Account("123456","pass@1234")
 print(a.acc_num)
 print(a.reset_pass()) # now it will orint the password
 print(a.__acc_pass)# print error 'Account' object has no attribute 'acc_pass'
+
+class Person:
+    __name = "hello" # creating a private class 
+
+    def __hello(): # creating a private object
+        print("hello world")
+
+p1 = Person()
+print(p1.__name)# it will also show an erroor because the __name ia a private attribute
