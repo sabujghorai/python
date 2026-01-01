1b577b39fa0d4363a4f3a882f78b0d0a046d3980
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-01-01 18:51:40 +0530

    Update oop.py

--- oop.py
+++ oop.py
@@ -201,3 +201,28 @@ c = int(input("Enter the marks of maths :"))
 
 s1 = Student(a,b,c)
 print(s1.percentage)
+
+# # polymorphism
+# the addition of complex numbers
+class Complex:
+    def __init__(self,real,img):
+        self.real = real
+        self.img = img
+
+    def showNumber(self):
+        print(self.real,"i +",self.img,"j")
+
+    def add(self,number2):
+        newReal = self.real + number2.real
+        newImg = self.img + number2.img
+        return Complex(newReal,newImg)
+
+
+num1 = Complex(1,3)
+num1.showNumber()
+
+num2 = Complex(3,5)
+num2.showNumber()
+
+num3 = num1.add(num2)
+num3.showNumber()
