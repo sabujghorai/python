e7b6ad11835419b63f6acbb7ceefe1978b440fc9
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-01-01 18:53:39 +0530

    Update oop.py
    using the dunder function to avoid the error

--- oop.py
+++ oop.py
@@ -212,7 +212,7 @@ class Complex:
     def showNumber(self):
         print(self.real,"i +",self.img,"j")
 
-    def add(self,number2):
+    def __add__(self,number2): # we are using the dunder function
         newReal = self.real + number2.real
         newImg = self.img + number2.img
         return Complex(newReal,newImg)
@@ -224,5 +224,6 @@ num1.showNumber()
 num2 = Complex(3,5)
 num2.showNumber()
 
-num3 = num1.add(num2)
+num3 = num1 + num2 # if we write this without using the dunder function it will show an error
+# num3 = num1.add(num2)
 num3.showNumber()
