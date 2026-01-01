ba862046957365d795ce0b275027ae1bd1f0b7a6
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-01-01 19:09:43 +0530

    Update oop.py
    we are implementing the division dunder function

--- oop.py
+++ oop.py
@@ -226,6 +226,11 @@ class Complex:
         newReal = self.real * number2.real
         newImg = self.img * number2.img
         return Complex(newReal,newImg)
+        
+    def __truediv__(self,number2): # here we are using the dunder multiplication function
+        newReal = self.real * number2.real
+        newImg = self.img * number2.img
+        return Complex(newReal,newImg)
     
 
 num1 = Complex(1,3)
@@ -235,6 +240,9 @@ num2 = Complex(3,5)
 num2.showNumber()
 
 print("---------")
-num3 = num1 + num2 # if we write this without using the dunder function it will show an error
+num3 = num1 + num2# if we write this without using the dunder function it will show an error
+num3 = num1 - num2
+num3 = num1 * num2
+num3 = num1 / num2
 # num3 = num1.add(num2)
 num3.showNumber()
