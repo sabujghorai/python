611609ecda593180d7ca35ceb5d4223d1c9397f0
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-01-01 20:17:55 +0530

    Update oop2.py

--- oop2.py
+++ oop2.py
@@ -173,3 +173,48 @@ c = int(input("Enter the marks of maths :"))
 
 s1 = Student(a,b,c)
 print(s1.percentage)
+
+# # polymorphism
+# the addition of complex numbers
+class Complex:
+    def __init__(self,real,img):
+        self.real = real
+        self.img = img
+
+    def showNumber(self):
+        print(self.real,"i +",self.img,"j")
+
+    def __add__(self,number2): # we are using the dunder function
+        newReal = self.real + number2.real
+        newImg = self.img + number2.img
+        return Complex(newReal,newImg)
+        
+    def __sub__(self,number2): # here we are using the dunder substraction function
+        newReal = self.real - number2.real
+        newImg = self.img - number2.img
+        return Complex(newReal,newImg)
+
+    def __mul__(self,number2): # here we are using the dunder multiplication function
+        newReal = self.real * number2.real
+        newImg = self.img * number2.img
+        return Complex(newReal,newImg)
+        
+    def __truediv__(self,number2): # here we are using the dunder multiplication function
+        newReal = self.real * number2.real
+        newImg = self.img * number2.img
+        return Complex(newReal,newImg)
+    
+
+num1 = Complex(1,3)
+num1.showNumber()
+
+num2 = Complex(3,5)
+num2.showNumber()
+
+print("---------")
+num3 = num1 + num2# if we write this without using the dunder function it will show an error
+num3 = num1 - num2
+num3 = num1 * num2
+num3 = num1 / num2
+# num3 = num1.add(num2)
+num3.showNumber()
