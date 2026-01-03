9feffcf0ba8cc760b4afefdc3a9728dff96b2bd5
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-01-03 10:44:11 +0530

    Update oop_practice.py

--- oop_practice.py
+++ oop_practice.py
@@ -16,3 +16,27 @@ a = int(input("Enter the radius :"))
 c2 = Circle(a)
 print(c2.Area())
 print(c2.Perimeter())
+
+# Define a Employee class with attribute role,department & salary.
+# the class also have a showDetail() method
+class Employee:
+    def __init__(self, role, dpt, salary):
+        self.role = role
+        self.dpt = dpt
+        self.salary = salary
+
+    def showDetail(self):
+        print("Role =", self.role)
+        print("Department =", self.dpt)
+        print("Salary =", self.salary)
+
+
+class Engineer(Employee):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        super().__init__("Engineer", "IT", 75000)
+
+
+engg1 = Engineer("Elon Musk", 45)
+engg1.showDetail()
