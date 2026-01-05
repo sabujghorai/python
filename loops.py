ebd2178a643cf98de346fb3135a7d1077fae267b
sabujghorai <ghoraisabuj4@gmail.com>
2026-01-05 18:07:38 +0530

    using the division as a updator

--- loops.py
+++ loops.py
@@ -1,24 +1,29 @@
-# basic syntax for a while loop
-count = 1
-while count <= 10:
-  a = "hello"
-  print(a,count)
-  count += 1
+# # basic syntax for a while loop
+# count = 1
+# while count <= 10:
+#   a = "hello"
+#   print(a,count)
+#   count += 1
 
-# printing 1 to 5 in increasing order
-i = 1
-while i<=5:
-  print(i)
-  i +=1
+# # printing 1 to 5 in increasing order
+# i = 1
+# while i<=5:
+#   print(i)
+#   i +=1
 
-    # printing 5 to 0 in decreasing order
-j = 5
-while j>=1:
-  print(j)
-  j-=1
-n = 5
-m = 10
-print(m+n)
-for i in range(0,n):
-    for j in range(0,m,2):
-        print("hello")
+#     # printing 5 to 0 in decreasing order
+# j = 5
+# while j>=1:
+#   print(j)
+#   j-=1
+# n = 5
+# m = 10
+# print(m+n)
+# for i in range(0,n):
+#     for j in range(0,m,2):
+#         print("hello")
+
+n = 10
+while(n >=1):
+    print("hello")
+    n = n//2
